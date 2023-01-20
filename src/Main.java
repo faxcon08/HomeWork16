bd4c67ae9e22182fcdb90523cbdf57aa3fdfd17d
faxcon08 <faxcon-08@mail.ru>
2023-01-20 15:55:40 +0300

    Second task 1.2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import lesson_16.*;
 
 public class Main {
+    static public void printSeparator() {
+        System.out.println("------------------------------------------------------------------------");
+    }
     public static void main(String[] args) {
         Car simpleCar1 = new Car("Lada", "Granta", 1.7, "Желтый", 2015,
                 "Россия", "ручная", "седан", "Н666СР777", 4, true , 105);
@@ -9,6 +12,7 @@ public class Main {
         Car simpleCar3 = new Car("BMW", "Z8", 3.0, "Красный", 2021,
                 "Германия", "автоматическая", "родстер", "С333СР341", 5, false,270);
 
+        printSeparator();
         System.out.println(simpleCar1);
         System.out.println(simpleCar2);
         System.out.println(simpleCar3);
@@ -18,6 +22,7 @@ public class Main {
         Bus simpleBus2 = new Bus("Зил","158", 1957, "USSR", "Green", 65, 65);
         Bus simpleBus3 = new Bus("Раф","251", 1955, "USSR", "White", 70, 40);
 
+        printSeparator();
         System.out.println(simpleBus1);
         System.out.println(simpleBus2);
         System.out.println(simpleBus3);
