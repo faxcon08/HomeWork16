0452603b92e53b4aad2d7b04f01c1ff62aaa2379
faxcon08 <faxcon-08@mail.ru>
2023-01-20 15:19:20 +0300

    First task 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import lesson_16.Car;
+import lesson_16.Transport;
 public class Main {
     public static void main(String[] args) {
+        Car simpleCar1 = new Car("Lada", "Granta", 1.7, "Желтый", 2015,
+                "Россия", "ручная", "седан", "Н666СР777", 4, true , 105);
+        Car simpleCar2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "Черный", 2020,
+                "Германия", "автоматическая", "седан", "О626СР347", 5, true,200);
+        Car simpleCar3 = new Car("BMW", "Z8", 3.0, "Красный", 2021,
+                "Германия", "автоматическая", "родстер", "С333СР341", 5, false,270);
 
+        System.out.println(simpleCar1);
+        System.out.println(simpleCar2);
+        System.out.println(simpleCar3);
     }
 }
--- src/lesson_16/Transport.java
+++ src/lesson_16/Transport.java
@@ -10,34 +10,58 @@ public class Transport {
     protected String bodyColor;
     protected int maxSpeed;
 
+    ////////////////////////////////// class for default values ///////////////////////////
+    protected static class Default_Values{
+        static String default_brand = "default_brand";
+        static String default_model = "default_model";
+        static int default_year = 2000;
+        static String default_country = "default_country";
+        static String default_Color = "default_white";
+        static int default_max_speed = 170;
+    } // Default_Values
+
     /////// constructor /////
     Transport(String brand,String model, int releaseYear, String productionCountry, String bodyColor, int maxSpeed) {
-        this.brand=brand;
-        this.model=model;
+
+
+        java.time.LocalDate date = java.time.LocalDate.now();
+        int currentYear = date.getYear();
+        releaseYear = (releaseYear>1920 && releaseYear<(currentYear+1))? releaseYear : Default_Values.default_year;
+
+        this.brand= simpleCheckString(brand,Default_Values.default_brand);
+        this.model= simpleCheckString(model,Default_Values.default_model);
         this.releaseYear=releaseYear;
-        this.productionCountry=productionCountry;
+        this.productionCountry= simpleCheckString(productionCountry,Default_Values.default_country);
         this.setBodyColor(bodyColor);
         this.setMaxSpeed(maxSpeed);
     }
+    /////// utility ////////
+
+    protected String simpleCheckString(String forCheck, String default_value) {
+        if(forCheck==null || forCheck.isBlank()) {
+            System.out.println("Некорректный ввод. Значение будет измененено на: "+default_value);
+            return default_value;
+        }
+        return forCheck.trim().replace("\\s+"," ");
+    }
+
     /////// setters ///////
 
     public void setBodyColor(String bodyColor) {
-        String default_body_color = "white(default)";
         if (bodyColor!=null && !bodyColor.isBlank()) {
             this.bodyColor = bodyColor;
         } else  {
-            this.bodyColor=default_body_color;
+            this.bodyColor=Default_Values.default_Color;
         }
 
     }
 
     public void setMaxSpeed(int maxSpeed) {
-        int default_max_speed = 70;
         if(maxSpeed>0 && maxSpeed<400){
             this.maxSpeed = maxSpeed;
         }
         else {
-            this.maxSpeed = default_max_speed;
+            this.maxSpeed = Default_Values.default_max_speed;
         }
     }
 
